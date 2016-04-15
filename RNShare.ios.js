107f935368c1930cc4035ddd30da090d952fb65b
Haruto0307 <skybolt0307@gmail.com>
2016-04-14 21:24:40 -0300

    upgrade react-native-share to version 0.23

---
+++ RNShare.ios.js
@@ -0,0 +1,23 @@
+/**
+ * @providesModule RNShare
+ * @flow
+ */
+'use strict';
+
+import {NativeModules} = from 'NativeModules';
+let NativeRNShare = NativeModules.RNShare;
+/**
+ * High-level docs for the RNShare iOS API can be written here.
+ */
+
+var RNShare = {
+  test: function() {
+
+    NativeRNShare.test();
+  },
+  open: function(options) {
+  	NativeRNShare.open(options);
+  }
+};
+
+module.exports = RNShare;
