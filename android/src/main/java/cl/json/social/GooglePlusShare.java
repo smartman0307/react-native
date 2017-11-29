f32b141256c555a3c11acd6ebb6f4c01377b5152
Haruto0307 <skybolt0307@gmail.com>
2017-11-29 11:55:15 -0800

    updating twitter package

--- android/src/main/java/cl/json/social/GooglePlusShare.java
+++ android/src/main/java/cl/json/social/GooglePlusShare.java
@@ -29,7 +29,7 @@ public class GooglePlusShare extends SingleShareIntent {
 
     @Override
     protected String getDefaultWebLink() {
-        return PLAY_STORE_LINK;
+        return null;
     }
 
     @Override
