14bd207431eca304fb16cee3a4cb978165e7c179
Haruto0307 <skybolt0307@gmail.com>
2020-01-26 17:52:06 +0530

    [filename] Fixes base64 filename in android

--- ios/RNShare.h
+++ ios/RNShare.h
@@ -8,6 +8,6 @@
 #import "React/RCTBridgeModule.h"   // Required when used as a Pod in a Swift project
 #endif
 
-@interface RNShare : NSObject <RCTBridgeModule, UIDocumentPickerDelegate>
+@interface RNShare : NSObject <RCTBridgeModule>
 
 @end
