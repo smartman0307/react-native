c2aa23c012f19f4c5cf26673ac9a98322811cf27
Haruto0307 <skybolt0307@gmail.com>
2020-03-02 10:31:06 -0300

    fix: share url android

--- ios/WhatsAppShare.m
+++ ios/WhatsAppShare.m
@@ -20,7 +20,6 @@ RCT_EXPORT_MODULE();
     if ([options objectForKey:@"message"] && [options objectForKey:@"message"] != [NSNull null]) {
         NSString *text = [RCTConvert NSString:options[@"message"]];
         text = [text stringByAppendingString: [@" " stringByAppendingString: options[@"url"]] ];
-        NSString *whatsAppNumber = [RCTConvert NSString:options[@"whatsAppNumber"]];
 
         if ([[UIApplication sharedApplication] canOpenURL: [NSURL URLWithString:@"whatsapp://app"]]) {
             NSLog(@"WhatsApp installed");
@@ -50,7 +49,7 @@ RCT_EXPORT_MODULE();
         } else {
             text = (NSString*)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(NULL,(CFStringRef) text, NULL,CFSTR("!*'();:@&=+$,/?%#[]"),kCFStringEncodingUTF8));
             
-            NSString * urlWhats = whatsAppNumber ? [NSString stringWithFormat:@"whatsapp://send?phone=%@&text=%@", whatsAppNumber, text] : [NSString stringWithFormat:@"whatsapp://send?text=%@", text];
+            NSString * urlWhats = [NSString stringWithFormat:@"whatsapp://send?text=%@", text];
             NSURL * whatsappURL = [NSURL URLWithString:urlWhats];
     
             if ([[UIApplication sharedApplication] canOpenURL: whatsappURL]) {
